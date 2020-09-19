3f8bce67e4f4992b696b494c63912580287db303
Alex Gintovt <alexandr.gintovt@gmail.com>
2020-09-19 12:40:31 +0100

    first commit

---
+++ src/test/java/stepdef/StepDefinition.java
@@ -0,0 +1,37 @@
+package stepdef;
+
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+public class StepDefinition {
+
+    @Given("^User is on NetBanking landing page$")
+    public void user_is_on_netbanking_landing_page() throws Throwable {
+        System.out.println("navigated to login url");
+    }
+
+    @When("^User login into application with \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void user_login_into_application_with_username_and_password(String strArg1, String strArg2) throws Throwable {
+        System.out.println(strArg1);
+        System.out.println(strArg2);
+    }
+
+    @Then("^Home page is populated$")
+    public void home_page_is_populated() throws Throwable {
+        System.out.println("validated home page");
+    }
+
+    @And("^Cards displayed are \"([^\"]*)\"$")
+    public void cards_displayed_are(String strArg3) throws Throwable {
+        System.out.println("cards displayed: " + strArg3);
+    }
+
+
+}
